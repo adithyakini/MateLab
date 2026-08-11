ff5d5fea9ecac824213bc0a573d97025fef954aa
adithyakini <67313225+adithyakini@users.noreply.github.com>
2026-08-11 13:43:22 +0530

    Fix worksheet import for Linux deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Practice from "./pages/Practice";
-import Worksheet from "./pages/Worksheet";
+import Home from "./pages/home";
+import Practice from "./pages/practice";
+import Worksheet from "./pages/worksheet";
 
 export default function App() {
 
